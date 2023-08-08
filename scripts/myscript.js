3beff1b29fa96f1dde81f5ad29671df50996ec3d
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-08-08 11:31:50 +0000

    Add classes to styles and jQuery classes to script

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    
+    $("h2").addClass("underline"); //Underlines all <h2> elements
+    $("ul").addClass("border"); //Adds a border to all lists
+});
+
+// Will remove that class from the elements 
+$(document).ready(function() {
+    $("h2").removeClass("underline"); //Underlines all <h2> elements
+    $("ul").removeClassClass("border"); //Adds a border to all lists
 });
